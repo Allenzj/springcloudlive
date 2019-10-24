ad9097c3da12038a2d5dcdedba701e64eaebfa6c
成都 彭磊 <leipengzj@163.com>
2019-10-24 14:18:20 +0800

    nacos学习

--- springcloud-eureka-client/src/main/java/com/bmsoft/controller/DemoController.java
+++ springcloud-eureka-client/src/main/java/com/bmsoft/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.bmsoft.controller;
 
+import java.io.File;
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,37 @@ public class DemoController {
 
   @RequestMapping("/info")
   public String getInfo() {
+    String path =
+        FileUtil.rootPath() + File.separator + "springcloud-eureka-client" + File.separator + "pdf";
+    InputStream stream = DemoController.class.getClassLoader()
+        .getResourceAsStream("pdf/a.pdf");
+    String path1 = DemoController.class.getClassLoader().getResource("pdf/a.pdf").getPath();
+    System.out.println(path1 + "----PDF模板文件的根路径：" + path);
+    String template = path + File.separator + "a.pdf";
+
+    File file = new File(template);
+    File path_file = new File(path1);
+    System.out.println(file + "====>" + file.exists());
+    System.out.println(path_file + "====>" + path_file.exists());
     return "eureka、zookeeper、consol的区别，集群端口--->" + port;
   }
 
+
+  @RequestMapping("/info2")
+  public String getInfo2() {
+    String path =
+        FileUtil.rootPath() + File.separator + "springcloud-eureka-client" + File.separator + "pdf";
+
+    System.out.println("PDF模板文件的根路径：" + path);
+    String template = path + File.separator + "b.pdf";
+    File file = new File(template);
+    System.out.println(file + "====>" + file.exists());
+//    try {
+//      PdfReader reader = new PdfReader(template);
+//      System.out.println("reader----" + reader);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+    return "eureka、zookeeper、consol的区别，集群端口--->" + port;
+  }
 }
